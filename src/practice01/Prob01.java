a0522d5b312f7ece1f71e95ee21378374bc196d9
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-19 16:24:33 +0900

    Prob01

--- src/practice01/Prob01.java
+++ src/practice01/Prob01.java
@@ -9,5 +9,10 @@ public class Prob01 {
 		Scanner scanner = new Scanner( System.in );
 		
 		int number = scanner.nextInt();
+		
+		if(number%3==0)System.out.println("3의 배수입니다.");
+		else System.out.println("3의 배수가 아닙니다.");
+		
+		scanner.close();
 	}
 }
