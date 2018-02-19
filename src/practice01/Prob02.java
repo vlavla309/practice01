c203619d2ba39212a93829478b92630cebdd4fc6
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-19 16:27:22 +0900

    prob2

--- src/practice01/Prob02.java
+++ src/practice01/Prob02.java
@@ -3,7 +3,12 @@ package practice01;
 public class Prob02 {
 
 	public static void main(String[] args) {
-
+		for(int i=1; i<10; i++){
+			for(int j=0; j<10; j++) {
+				System.out.print(i+j+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
